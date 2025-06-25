0469a8a8610ff5cb6efece7e750001ce5ff585f3
whimandwonder <connect.whimandwonder@gmail.com>
2025-06-25 11:06:40 +0530

    feat: Configure sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import sitemap from 'vite-plugin-sitemap'; // 1. Import the sitemap plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // 2. Add the sitemap plugin to your plugins array
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://whimandwonder.in',
+    }),
+  ],
   css: {
     postcss: {
       plugins: [
